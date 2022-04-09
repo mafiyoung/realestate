ab6534ac96c87d92f5e0f51db2c720ff711a832a
mafiyoung <65676596+mafiyoung@users.noreply.github.com>
2022-04-09 12:28:41 -0700

    reinitial commit

--- btre/static/js/main.js
+++ btre/static/js/main.js
@@ -2,5 +2,5 @@ const date = new Date();
 document.querySelector('.year').innerHTML = date.getFullYear();
 
 setTimeout(function() {
-    $('#message').fadeOut('slow');
+   $('#message').fadeOut('slow');
 },3000);
